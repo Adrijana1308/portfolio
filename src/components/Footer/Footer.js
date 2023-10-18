c5a211c9de3e011001aef9325b980e105d9e8bfd
Adrijana1308 <adrijanababic4@gmail.com>
2023-10-19 00:06:34 +0200

    footer update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,11 +12,11 @@ function Footer() {
     <div className={`Footer ${isOn ? 'dark-mode' : ''}`}>
         <h2>adrijanababic4@gmail.com</h2>
         <div className='footer-links'>
-            <Link to="about" spy={true} smooth={true} duration={500} className='footer-links'>About</Link>
-            <Link to="skills" spy={true} smooth={true} duration={500} className='footer-links'>Skills</Link>
-            <Link to="experience" spy={true} smooth={true} duration={500} className='footer-links'>Experience</Link>
-            <Link to="projects" spy={true} smooth={true} duration={500} className='footer-links'>Projects</Link>
-            <Link to="education" spy={true} smooth={true} duration={500} className='footer-links'>Education</Link>
+            <Link to="about" spy={true} smooth={true} duration={500} className={`footer-links ${isOn ? 'dark-mode' : ''}`}>About</Link>
+            <Link to="skills" spy={true} smooth={true} duration={500} className={`footer-links ${isOn ? 'dark-mode' : ''}`}>Skills</Link>
+            <Link to="experience" spy={true} smooth={true} duration={500} className={`footer-links ${isOn ? 'dark-mode' : ''}`}>Experience</Link>
+            <Link to="projects" spy={true} smooth={true} duration={500} className={`footer-links ${isOn ? 'dark-mode' : ''}`}>Projects</Link>
+            <Link to="education" spy={true} smooth={true} duration={500} className={`footer-links ${isOn ? 'dark-mode' : ''}`}>Education</Link>
         </div>
     </div>
   )
