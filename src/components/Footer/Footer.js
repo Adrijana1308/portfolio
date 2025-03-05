da769f5674d84520fcd0062ff7c7056c554072fb
Adrijana1308 <adrijanababic4@gmail.com>
2025-03-06 00:12:02 +0100

    Icon update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -56,7 +56,7 @@ function Footer() {
         />
         <img
           className="icons"
-          src="https://www.svgrepo.com/show/374114/symfony.svg"
+          src="https://www.svgrepo.com/show/372958/symfony-badge.svg"
           alt="Symfony icon"
         />
         <img
